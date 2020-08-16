8ecc50cc7c8500c6b399851ce9447621ee3ae110
Herman Duijker <herman.duijker@starstone.info>
2020-08-16 22:15:16 +0200

    Setting up

---
+++ FundaListAppTests/TestFundaAPI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+
+namespace FundaListAppTests
+{
+    /// <summary>
+    /// Test the Funda API
+    /// </summary>
+    public class TestFundaApi
+    {
+        [Fact]
+        public void RetrieveSingleItem()
+        {
+            // Arrange
+            var target = new FundaApi();
+
+            // Act
+            var result = target.Query(searchType, "/amsterdam/tuin", pageSize);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count() > 0);
+        }
+
+        [Fact]
+        public void RetrieveSinglePage()
+        {
+            // Arrange
+            var target = new FundaApi();
+
+            // Act
+            var result = target.Query(searchType, "/amsterdam/tuin", pageSize);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count() > 0);
+        }
+    }
+}
